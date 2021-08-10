748948e6fd53f6286a4420b923a9a6cc54175012
Hieu <xuanhieu0012@gmail.com>
2021-08-09 19:24:07 -0500

    commit tree 4b8df0f73a6a6a1fd7547b4bc208d25b24ca8125

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+function saturdayFun(toDo = "roller-skate") {
+    return(`This Saturday, I want to ${toDo}!`);
+
+}
+function mondayWork(work = "go to the office") {
+    return(`This Monday, I will ${work}.`);
+}
+
+function wrapAdjective(string="*"){
+    return function(thing ="special"){
+
+        return `You are ${string}${thing}${string}!`;
+    }
+}
